abc385519cf00dd0412cec83ac22efd1d9d7738f
ignaciolarranaga <ignaciolarranaga@gmail.com>
2022-12-11 11:31:18 -0300

    Improving the deployment by avoiding libraries processing.

--- packages/stack/src/lambda/graphql-resolvers.ts
+++ packages/stack/src/lambda/graphql-resolvers.ts
@@ -23,17 +23,7 @@ export function buildGraphQLResolversLambda(
   const librariesLayer = new LayerVersion(scope, 'GraphQLResolversLibraries', {
     layerVersionName: `example-graphql-resolvers-libraries-${env}`,
     description: 'The GraphQL resolvers libraries layer',
-    code: Code.fromAsset('../graphql-resolvers-libraries', {
-      bundling: {
-        image: Runtime.NODEJS_16_X.bundlingImage,
-        // We need to introduce the nodejs directory required for the lambda layer
-        command: [
-          'bash',
-          '-c',
-          'mkdir /asset-output/nodejs && cp -r /asset-input/* /asset-output/nodejs',
-        ],
-      },
-    }),
+    code: Code.fromAsset('../graphql-resolvers-libraries'),
     compatibleRuntimes: [Runtime.NODEJS_16_X], // spellchecker: disable-line
   });
 
