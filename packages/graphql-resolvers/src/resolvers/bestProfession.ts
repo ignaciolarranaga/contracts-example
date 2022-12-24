eb2bf3f809782b406436a3f8af2181a64abbe9dd
ignaciolarranaga <ignaciolarranaga@gmail.com>
2022-12-24 12:34:28 -0300

    Minor fix.

--- packages/graphql-resolvers/src/resolvers/bestProfession.ts
+++ packages/graphql-resolvers/src/resolvers/bestProfession.ts
@@ -30,7 +30,7 @@ export default async function bestProfession(
             : '0000-01-01T00:00:00.000Z',
           ':end': event.arguments.filter?.end
             ? event.arguments.filter.end
-            : '9999-12-31T23:59:69.999Z',
+            : '9999-12-31T23:59:59.999Z',
         },
       })
       .promise()
